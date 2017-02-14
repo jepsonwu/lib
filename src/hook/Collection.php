6ba78e2a2dd8268b7a8fbb64d0ed75ac8edceb80
shanzha <shanzha@in66.com>
2017-02-14 14:21:25 +0800

    hook test

---
+++ src/hook/Collection.php
@@ -0,0 +1,13 @@
+<?php
+namespace Jepsonwu\Hook;
+/**
+ * getHooks
+ * Created by PhpStorm.
+ * User: jepsonwu
+ * Date: 2017/2/14
+ * Time: 10:23
+ */
+interface Collection
+{
+    public static function getHooks();
+}
